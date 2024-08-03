915ccd091f0521773cea4a2a5766c81e178decd8
veeeeeeee00 <maxim.w.95@mail.ru>
2024-08-03 10:48:14 +0200

    [add] music note with random links

--- scripts.js
+++ scripts.js
@@ -5,12 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
         { text: "note: near a tree by a river, there's a hole in the ground..." },
         { text: "note: :3" },
         { text: ":)", url: "/media/mystery.mp4" },
+        { text: "note: yeah i know that the github button isn't aligned, i'll fix it later." },
+        { text: "tip: did you know? refreshing this site changes this text." },
+        { text: "tip: don't mine at night" },
+        { text: "note: well here we are again, it's always such a pleasure..." },
+        { text: "note: huge success" },
+
     ];
 
     var randomMessage = messages[Math.floor(Math.random() * messages.length)];
 
     var footer = document.getElementById('footer');
-    footer.textContent = '';
+    footer.innerHTML = '<input class="noteMS" type="image" height="30" width="30" src="/media/music.png" name="B1" onclick="randomlink()"><span class="music-note"></span>';
 
     if (randomMessage.url) {
         var link = document.createElement('a');
@@ -19,6 +25,16 @@ document.addEventListener('DOMContentLoaded', function() {
         link.style.color = "white"; // Change the color if needed
         footer.appendChild(link);
     } else {
-        footer.textContent = randomMessage.text;
+        footer.appendChild(document.createTextNode(randomMessage.text));
     }
 });
+
+var randomlinks = [];
+randomlinks[0] = "https://www.youtube.com/watch?v=yGZr98GEs0U";
+randomlinks[1] = "https://www.youtube.com/watch?v=AS58aeJQI4Y";
+randomlinks[2] = "https://www.youtube.com/watch?v=cygu65ytwTc";
+randomlinks[3] = "https://www.youtube.com/watch?v=M9J6DKJXoKk";
+
+function randomlink() {
+    window.open(randomlinks[Math.floor(Math.random() * randomlinks.length)]);
+}
