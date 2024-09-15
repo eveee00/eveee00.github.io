57b0cc0dc8eaab25dbab6e184875f1b84634feb3
eveee00 <maxim.w.008@gmail.com>
2024-09-15 11:10:11 +0200

    somewhat fixed mobile view

--- scripts.js
+++ scripts.js
@@ -85,3 +85,28 @@ function openTab(evt, cityName) {
         document.querySelector('.tab button.tablinks').className += " active";
     }
 }
+
+function openTabFromDropdown(event) {
+    const tabName = event.target.value;
+    openTab(null, tabName);
+}
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+
+    if (evt) {
+        evt.currentTarget.className += " active";
+    }
+}
