f48d2e928eeaa9d4b420209f2f67ef3616309acf
eveee00 <83956322+eveee00@users.noreply.github.com>
2025-09-21 13:07:51 +0200

    add back lanyard status

--- scripts.js
+++ scripts.js
@@ -1,10 +1,56 @@
+function updateNowPlayingText(data) {
+    const nowPlaying = document.getElementById("nowPlaying");
+    if (!nowPlaying) return;
+    const { discord_status, activities } = data;
+    if (activities && activities.length > 0) {
+        const musicPlayer = activities.find(a => a.type === 2);
+        if (musicPlayer) {
+            nowPlaying.innerHTML = `<p>I'm currently listening to <a href="${musicPlayer.details_url}">${musicPlayer.details}</a> <a href="${musicPlayer.state_url}">${musicPlayer.state}</a> on ${musicPlayer.name}</p>`;
+            return;
+        }
+        const game = activities.find(a => a.type === 0);
+        if (game) {
+            nowPlaying.innerHTML = `<p>I'm currently playing ${game.name}</p>`;
+            return;
+        }
+    }
+
+    if (discord_status === "online") {
+        nowPlaying.innerHTML = "<p>I'm online, but not doing anything!</p>";
+    } else if (discord_status === "offline") {
+        nowPlaying.innerHTML = "<p>I'm currently offline!</p>";
+    } else if (discord_status === "idle") {
+        nowPlaying.innerHTML = "<p>I'm online, but away!</p>";
+    } else {
+        nowPlaying.innerHTML = "Loading...";
+    }
+}
+
+function setLanyardStatus() {
+    fetch("https://api.lanyard.rest/v1/users/675299056070688789")
+        .then(response => response.json())
+        .then(json => {
+            if (json && json.success && json.data) {
+                updateNowPlayingText(json.data);
+            }
+        })
+        .catch(() => {
+            const nowPlaying = document.getElementById("nowPlaying");
+            if (nowPlaying) nowPlaying.innerHTML = "cannot update nowPlaying!";
+        });
+}
+
+addEventListener("DOMContentLoaded", () => {
+    setLanyardStatus();
+});
+
+
 console.log(`
      _____ 
  _  |___ / 
 (_)   |_ \\ 
  _   ___) |
 (_) |____/ 
-
 
 mrooooow~
 `);
