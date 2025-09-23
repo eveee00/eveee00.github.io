671891cdb87bcc7e516a7e96573eb978c0714ad2
eveee00 <83956322+eveee00@users.noreply.github.com>
2025-09-23 11:04:55 +0200

    d

--- scripts.js
+++ scripts.js
@@ -17,7 +17,6 @@ function updateNowPlayingText(data) {
 
     if (discord_status === "online") {
         nowPlaying.innerHTML = "<p>I'm online, but not doing anything!</p>";
-        
     } else if (discord_status === "offline") {
         nowPlaying.innerHTML = "<p>I'm currently offline!</p>";
     } else if (discord_status === "idle") {
